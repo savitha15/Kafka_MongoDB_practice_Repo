566dc2bb77dfff653f4f6e0f1de3573f8c39c408
savitha15 <17euec135@skcet.ac.in>
2022-06-05 13:17:07 +0530

    spring core practice

---
+++ src/main/java/com/springcore/autowire/Emp.java
@@ -0,0 +1,24 @@
+package com.springcore.autowire;
+
+public class Emp {
+	
+	private Address address;
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Emp(Address address) {
+		super();
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
